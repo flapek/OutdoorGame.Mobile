c7c2edd5701a51fa85b709becbe2a2327f23344b
flapek <filap4@gmail.com>
2020-11-26 21:31:11 +0100

    clean code

--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -4,8 +4,6 @@ import { StyleSheet } from 'react-native';
 import GameComponent from '../components/GameComponent';
 import { View } from '../components/Themed';
 
-import GameRepository from '../Repositories/GameRepository'
-
 export default function MainScreen() {
   return (
     <View style={styles.container}>
@@ -13,21 +11,11 @@ export default function MainScreen() {
     </View>
   );
 }
-var repository = new GameRepository();
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
