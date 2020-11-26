b28ff41aa090de86b325c21c8e1828283c5167ae
flapek <filap4@gmail.com>
2020-11-26 09:48:09 +0100

    update view

--- view/GameView.tsx
+++ view/GameView.tsx
@@ -2,25 +2,23 @@ import React from 'react'
 import { View, Text, FlatList, TouchableOpacity, ListRenderItemInfo } from 'react-native';
 import IGame from '../models/interfaces/IGame';
 
-function GameView(data: IGame[]): JSX.Element {
+export default function GameView({ data } : {data: IGame[]}): JSX.Element {
     return (
         <View>
             <FlatList
                 data={data}
-                // ItemSeparatorComponent={FlatListItemSeparator}
-                renderItem={item => renderItem(item)}
+                ItemSeparatorComponent={() => ( <View></View> )}
+                renderItem={({ item }) => renderItem(item)}
                 keyExtractor={item => item.id.toString()} />
         </View>
     );
 }
 
-function renderItem(data: ListRenderItemInfo<IGame>) : JSX.Element {
+function renderItem(item: IGame): JSX.Element{
     return (
         <TouchableOpacity>
-            <Text>{data.item.id}</Text>
-            <Text>{data.item.name}</Text>
+            <Text>{item.id}</Text>
+            <Text>{item.name}</Text>
         </TouchableOpacity>
     );
 }
-
-export default GameView;
